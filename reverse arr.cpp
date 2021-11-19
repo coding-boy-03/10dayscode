0c6950c302723102cb1b6099661d87ad6690ed54
Abhishek L <84949557+codingboy03@users.noreply.github.com>
2021-11-19 18:24:59 +0530

    today is done

---
+++ reverse arr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of integer in array"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elemnts of array"<<endl;
+    for(int i =0; i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"The reverse order of the given array is "<<endl;
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
