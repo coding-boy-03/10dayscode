7ed343d570884953ac0fec0c8449875ee978850e
Abhishek L <84949557+codingboy03@users.noreply.github.com>
2022-03-14 06:05:01 +0530

    Create AVL tree.c

---
+++ AVL tree.c
@@ -0,0 +1,186 @@
+ 
+ ​#​include​ ​<​stdio.h​> 
+ ​#​include​<​stdlib.h​> 
+ ​typedef​ ​struct​ node 
+ ​{ 
+ ​int​ data; 
+ ​struct​ node *left,*right; 
+ ​int​ ht; 
+ ​}node; 
+  
+ ​node *​insert​(node *,​int​); 
+ ​void​ ​inorder​(node *); 
+ ​int​ ​height​( node *); 
+ ​node *​rotateright​(node *); 
+ ​node *​rotateleft​(node *); 
+ ​node *​RR​(node *); 
+ ​node *​LL​(node *); 
+ ​node *​LR​(node *); 
+ ​node *​RL​(node *); 
+ ​int​ ​BF​(node *); 
+ ​int​ ​main​() 
+ ​{ 
+ ​node *root=​NULL​; 
+ ​int​ x,n,i,op; 
+ ​do 
+ ​{ 
+ ​printf​(​"​\n​1)Create:​"​); 
+ ​printf​(​"​\n​2)Insert:​"​); 
+ ​printf​(​"​\n​3)Delete:​"​); 
+ ​printf​(​"​\n​4)Print:​"​); 
+ ​printf​(​"​\n​5)Quit:​"​); 
+ ​printf​(​"​\n\n​Enter Your Choice:​"​); 
+ ​scanf​(​"​%d​"​,&op); 
+ ​switch​(op) 
+ ​{ 
+ ​case​ ​1​: ​printf​(​"​\n​Enter no. of elements:​"​); 
+ ​scanf​(​"​%d​"​,&n); 
+ ​printf​(​"​\n​Enter tree data:​"​); 
+ ​root=​NULL​; 
+ ​for​(i=​0​;i<n;i++) 
+ ​{ 
+ ​scanf​(​"​%d​"​,&x); 
+ ​root=​insert​(root,x); 
+ ​} 
+ ​break​; 
+ ​case​ ​2​: ​printf​(​"​\n​Enter a data:​"​); 
+ ​scanf​(​"​%d​"​,&x); 
+ ​root=​insert​(root,x); 
+ ​break​; 
+ ​case​ ​3​: ​printf​(​"​\n​Enter a data:​"​); 
+ ​scanf​(​"​%d​"​,&x); 
+ ​break​; 
+ ​case​ ​4​: 
+ ​printf​(​"​\n\n​Inorder sequence:​\n​"​); 
+ ​inorder​(root); 
+ ​printf​(​"​\n​"​); 
+ ​break​; 
+ ​} 
+ ​}​while​(op!=​5​); 
+ ​return​ ​0​; 
+ ​} 
+  
+ ​node * ​insert​(node *T,​int​ x) 
+ ​{ 
+ ​if​(T==​NULL​) 
+ ​{ 
+ ​T=(node*)​malloc​(​sizeof​(node)); 
+ ​T->​data​=x; 
+ ​T->​left​=​NULL​; 
+ ​T->​right​=​NULL​; 
+ ​} 
+ ​else 
+ ​if​(x > T->​data​) ​//​ insert in right subtree 
+ ​{ 
+ ​T->​right​=​insert​(T->​right​,x); 
+ ​if​(​BF​(T)==-​2​) 
+ ​if​(x>T->​right​->​data​) 
+ ​T=​RR​(T); 
+ ​else 
+ ​T=​RL​(T); 
+ ​} 
+ ​else 
+ ​if​(x<T->​data​) 
+ ​{ 
+ ​T->​left​=​insert​(T->​left​,x); 
+ ​if​(​BF​(T)==​2​) 
+ ​if​(x < T->​left​->​data​) 
+ ​T=​LL​(T); 
+ ​else 
+ ​T=​LR​(T); 
+ ​} 
+ ​T->​ht​=​height​(T); 
+ ​return​(T); 
+ ​} 
+  
+ ​int​ ​height​(node *T) 
+ ​{ 
+ ​int​ lh,rh; 
+ ​if​(T==​NULL​) 
+ ​return​(​0​); 
+ ​if​(T->​left​==​NULL​) 
+ ​lh=​0​; 
+ ​else 
+ ​lh=​1​+T->​left​->​ht​; 
+ ​if​(T->​right​==​NULL​) 
+ ​rh=​0​; 
+ ​else 
+ ​rh=​1​+T->​right​->​ht​; 
+ ​if​(lh>rh) 
+ ​return​(lh); 
+ ​return​(rh); 
+ ​} 
+  
+ ​node * ​rotateright​(node *x) 
+ ​{ 
+ ​node *y; 
+ ​y=x->​left​; 
+ ​x->​left​=y->​right​; 
+ ​y->​right​=x; 
+ ​x->​ht​=​height​(x); 
+ ​y->​ht​=​height​(y); 
+ ​return​(y); 
+ ​} 
+  
+ ​node * ​rotateleft​(node *x) 
+ ​{ 
+ ​node *y; 
+ ​y=x->​right​; 
+ ​x->​right​=y->​left​; 
+ ​y->​left​=x; 
+ ​x->​ht​=​height​(x); 
+ ​y->​ht​=​height​(y); 
+ ​return​(y); 
+ ​} 
+  
+ ​node * ​RR​(node *T) 
+ ​{ 
+ ​T=​rotateleft​(T); 
+ ​return​(T); 
+ ​} 
+  
+ ​node * ​LL​(node *T) 
+ ​{ 
+ ​T=​rotateright​(T); 
+ ​return​(T); 
+ ​} 
+  
+ ​node * ​LR​(node *T) 
+ ​{ 
+ ​T->​left​=​rotateleft​(T->​left​); 
+ ​T=​rotateright​(T); 
+ ​return​(T); 
+ ​} 
+  
+ ​node * ​RL​(node *T) 
+ ​{ 
+ ​T->​right​=​rotateright​(T->​right​); 
+ ​T=​rotateleft​(T); 
+ ​return​(T); 
+ ​} 
+  
+ ​int​ ​BF​(node *T) 
+ ​{ 
+ ​int​ lh,rh; 
+ ​if​(T==​NULL​) 
+ ​return​(​0​); 
+ ​if​(T->​left​==​NULL​) 
+ ​lh=​0​; 
+ ​else 
+ ​lh=​1​+T->​left​->​ht​; 
+ ​if​(T->​right​==​NULL​) 
+ ​rh=​0​; 
+ ​else 
+ ​rh=​1​+T->​right​->​ht​; 
+ ​return​(lh-rh); 
+ ​} 
+  
+ ​void​ ​inorder​(node *T) 
+ ​{ 
+ ​if​(T!=​NULL​) 
+ ​{ 
+ ​inorder​(T->​left​); 
+ ​printf​(​"​%d​(Bf=​%d​)​"​,T->​data​,​BF​(T)); 
+ ​inorder​(T->​right​); 
+ ​} 
+ ​}
